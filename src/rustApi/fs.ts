a285c770a6b31e8abd52e497f3a57887b820f501
Matthew Bryant <matt23488@gmail.com>
2022-12-22 16:40:15 -0500

    refactored fsPlugin (frontend): - hid directory browsing events/calls from consumers - created async function for initiating a browse directory request

--- src/rustApi/fs.ts
+++ src/rustApi/fs.ts
@@ -2,11 +2,14 @@ import { getInvokeFn, getListenFn, getOnceFn, RustApiTypes } from './_types';
 
 const plugin = 'tauri-plugin-fs';
 
-type PluginApi = {
+type PluginApi = {};
+type PluginEvents = {};
+
+type HiddenPluginApi = {
     browse_directory: RustApiTypes;
 };
 
-type PluginEvents = {
+type HiddenPluginEvents = {
     directory_selected: string | undefined;
 };
 
@@ -14,7 +17,11 @@ export const invoke = getInvokeFn<PluginApi>(plugin);
 export const listen = getListenFn<PluginEvents>(plugin);
 export const once = getOnceFn<PluginEvents>(plugin);
 
+const hiddenInvoke = getInvokeFn<HiddenPluginApi>(plugin);
+const hiddenListen = getListenFn<HiddenPluginEvents>(plugin);
+const hiddenOnce = getOnceFn<HiddenPluginEvents>(plugin);
+
 export const browseDirectory = () => new Promise<string | undefined>(resolve => {
-    once('directory_selected', event => resolve(event.payload));
-    invoke('browse_directory');
+    hiddenOnce('directory_selected', event => resolve(event.payload));
+    hiddenInvoke('browse_directory');
 });
